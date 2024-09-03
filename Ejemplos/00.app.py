58caa5a49ccf256ba38ba6bffa2b7780de9f2761
Jose Luis <luis2022luis@gmail.com>
2024-09-03 00:53:04 +0000

    UPDATED 00.app.py

--- Ejemplos/00.app.py
+++ Ejemplos/00.app.py
@@ -1,8 +1,22 @@
-import requests
+"""
+curl -X POST http://localhost:11434/api/generate -d '{
+  "model": "tinyllama",
+  "prompt": "Why is the sky blue?",
+  "stream": false
+}'
+"""
 
-url = 'https://www.w3schools.com/python/demopage.php'
-myobj = {'somekey': 'somevalue'}
+import requests
+import json
+
+url = 'http://localhost:11434/api/generate'
+myobj = {
+   "model": "tinyllama",
+   "prompt": "Why is the sky blue?",
+   "stream": False
+}
 
 x = requests.post(url, json = myobj)
+x= json.loads(x.text)
 
-print(x.text)
+print(x["response"])
